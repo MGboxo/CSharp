78f62302c62648ec4380c6a4622d79c0546d21a0
Mateusz Góźdź <mgboxo@gmail.com>
2018-05-16 20:09:27 +0200

    fix 0.01

--- FirstCSharp/FirstCSharp/Program.cs
+++ FirstCSharp/FirstCSharp/Program.cs
@@ -10,25 +10,30 @@ namespace FirstCSharp
     {
         static void Main(string[] args)
         {
-         //############################################
+            //############################################
+
             //Lesson 1
-            Console.WriteLine("Cerber");
+            Console.WriteLine("Hello");
             Console.WriteLine(DateTime.Now);
             //############################################
+
             //Lesson 2
             //Brak użytkowania przestrzeni nazw System
-
             //System.Console.WriteLine("Cerber");
             //System.Console.WriteLine(DateTime.Now);
             //############################################
+
             //Lesson 3
             //String Name = args[0];
             //Console.WriteLine("Witaj, " + Name);
+            //############################################
+
             //Lesson 3.5
             foreach (var item in args)
             {
                 Console.WriteLine("witaj, " + item);
             }
+            //############################################
         }
     }
 }
